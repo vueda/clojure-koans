3d3d290b289edd6934c9ffb6049621d16080fce9
Vinícius Ueda <vnsueda@gmail.com>
2019-09-05 22:36:48 -0300

    creating fn + recursion

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,29 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [rest-coll coll
+         acc '()]
+    (if (empty? rest-coll)
+      acc
+      (recur (rest rest-coll) (conj acc (first rest-coll))))))
 
-(defn factorial [n]
-  __)
+(defn factorial
+  ([n] (factorial n 1))
+  ([n f]
+   (if (<= n 1)
+     f
+     (recur (dec n) (* f n)))))
 
 (meditations
   "Recursion ends with a base case"
